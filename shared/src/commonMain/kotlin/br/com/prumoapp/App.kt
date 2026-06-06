27ab109b2b262db2fa3528aa5af1bf1de4a3d2f8
gitdaniellopes <danieljfsantos@gmail.com>
2026-06-06 12:01:54 -0300

    feat: setup Koin DI with modules for data source, repository, use case, and view model

--- shared/src/commonMain/kotlin/br/com/prumoapp/App.kt
+++ shared/src/commonMain/kotlin/br/com/prumoapp/App.kt
@@ -2,12 +2,29 @@ package br.com.prumoapp
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import br.com.prumoapp.di.dataSourceModule
+import br.com.prumoapp.di.repositoryModule
+import br.com.prumoapp.di.useCaseModule
+import br.com.prumoapp.di.viewModelModule
 import br.com.prumoapp.ui.theme.PrumoAppTheme
+import org.koin.compose.KoinApplication
+import org.koin.dsl.koinConfiguration
 
 @Composable
 @Preview
 fun App() {
-    PrumoAppTheme {
 
-    }
+    KoinApplication(configuration = koinConfiguration(declaration = {
+        modules(
+            dataSourceModule,
+            repositoryModule,
+            useCaseModule,
+            viewModelModule
+        )
+    }), content = {
+
+        PrumoAppTheme {
+
+        }
+    })
 }
---
+++ shared/src/commonMain/kotlin/br/com/prumoapp/di/ViewModelModule.kt
@@ -0,0 +1,5 @@
+package br.com.prumoapp.di
+
+import org.koin.dsl.module
+
+val viewModelModule = module { }
