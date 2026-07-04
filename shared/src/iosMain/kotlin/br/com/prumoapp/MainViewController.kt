006824dc061f42b546e764f85c0d54efc57f0d01
gitdaniellopes <danieljfsantos@gmail.com>
2026-07-04 12:25:57 -0300

    refactor: simplify MainViewController by removing GoogleSignIn dependency

--- shared/src/iosMain/kotlin/br/com/prumoapp/MainViewController.kt
+++ shared/src/iosMain/kotlin/br/com/prumoapp/MainViewController.kt
@@ -1,10 +1,5 @@
 package br.com.prumoapp
 
 import androidx.compose.ui.window.ComposeUIViewController
-import br.com.prumoapp.data.auth.GoogleSignInHelper
-import br.com.prumoapp.data.auth.GoogleSignInRegistry
 
-fun MainViewController(googleSignInHelper: GoogleSignInHelper) = ComposeUIViewController {
-    GoogleSignInRegistry.helper = googleSignInHelper
-    App()
-}
+fun MainViewController() = ComposeUIViewController { App() }
