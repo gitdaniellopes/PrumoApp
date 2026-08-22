0e4b134706ed64b34197fa2c5745a135e858aa63
Daniel Lopes <danieljfsantos@gmail.com>
2026-08-22 12:08:56 -0300

    login com google ios

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,5 +1,7 @@
 import SwiftUI
+import GoogleSignIn
 import FirebaseCore
+import Firebase
 import Shared
 
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -10,10 +12,22 @@ class AppDelegate: NSObject, UIApplicationDelegate {
       AppLogger.shared.initializedDebug()
       #endif
       
-    FirebaseApp.configure()
+      FirebaseApp.configure()
+      
+      if let clientId = FirebaseApp.app()?.options.clientID {
+          GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: clientId)
+      }
 
     return true
   }
+    
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool{
+        return GIDSignIn.sharedInstance.handle(url)
+    }
 }
 
 @main
